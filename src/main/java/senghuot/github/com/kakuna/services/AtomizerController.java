75dd7997970d37aa135c27251bb4553612c1df43
Josh Chang <coralbue@gmail.com>
2020-04-30 10:39:22 -0700

    add intial db code that don't work

--- src/main/java/senghuot/github/com/kakuna/services/AtomizerController.java
+++ src/main/java/senghuot/github/com/kakuna/services/AtomizerController.java
@@ -2,8 +2,9 @@ package senghuot.github.com.kakuna.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import senghuot.github.com.kakuna.Repo.TinyUrlRepo;
+import senghuot.github.com.kakuna.model.TinyURL;
 
 @RestController
 public class AtomizerController {
@@ -11,9 +12,24 @@ public class AtomizerController {
     @Autowired
     Environment environment;
 
+    @Autowired
+    TinyUrlRepo repo;
+
     @RequestMapping(value = "/tst")
     public String tst() {
         return "Atomizer instance: " + environment.getProperty("local.server.port");
     }
 
+    @GetMapping(value = "/{tinyUrl}")
+    public String getURL(@PathVariable("tinyUrl")String tinyUrl) {
+        return "http://google.com";
+    }
+
+    @PostMapping(value = "/", consumes = {"application/json"})
+    public String addURL(@RequestBody String url) {
+        String shortURL = url.toUpperCase();
+        TinyURL tinyURL = new TinyURL(shortURL, url);
+        repo.save(tinyURL);
+        return shortURL;
+    }
 }
