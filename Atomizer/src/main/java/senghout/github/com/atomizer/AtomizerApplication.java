9f493eee7571d2673e9c55287dd603d0d3e803dd
Josh Chang <coralbue@gmail.com>
2020-05-02 09:46:39 -0700

    separate the projects

---
+++ Atomizer/src/main/java/senghout/github/com/atomizer/AtomizerApplication.java
@@ -0,0 +1,13 @@
+package senghout.github.com.atomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomizerApplication.class, args);
+	}
+
+}
