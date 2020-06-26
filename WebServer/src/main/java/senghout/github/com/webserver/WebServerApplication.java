0f7091c4947f20850e0a7eca002dcca8d06f3c21
Josh Chang <coralbue@gmail.com>
2020-06-25 17:20:35 -0700

    Implemented rate limiting (#24)
    * got rate limiting working
    
    * clean up white space

--- WebServer/src/main/java/senghout/github/com/webserver/WebServerApplication.java
+++ WebServer/src/main/java/senghout/github/com/webserver/WebServerApplication.java
@@ -2,6 +2,7 @@ package senghout.github.com.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(useDefaultFilters = false)
+@EnableCaching
 public class WebServerApplication {
 
 	public static void main(String[] args) {
