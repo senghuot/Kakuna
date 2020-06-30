c9d00ba5a01628e6e260aa043f28b182d1000ae8
Scott L <senghuotlim@gmail.com>
2020-06-29 21:57:53 -0700

    Adding Skaffold (#25)
    * Adding Skaffold to speed up development. Removed Registration in favor of Kubernetes.
    
    * Adding Skaffold to speed up development. Removed Registration in favor of Kubernetes.
    
    * removing dummy rest api

--- Atomizer/src/main/java/senghout/github/com/atomizer/AtomizerApplication.java
+++ Atomizer/src/main/java/senghout/github/com/atomizer/AtomizerApplication.java
@@ -2,12 +2,7 @@ package senghout.github.com.atomizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class AtomizerApplication {
 	public static void main(String[] args) {
--- Atomizer/src/main/java/senghout/github/com/atomizer/AtomizerController.java
+++ Atomizer/src/main/java/senghout/github/com/atomizer/AtomizerController.java
@@ -1,9 +1,7 @@
 package senghout.github.com.atomizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import senghout.github.com.atomizer.model.AddUrlInput;
 import senghout.github.com.atomizer.model.TinyUrl;
 import senghout.github.com.atomizer.model.Zoo;
